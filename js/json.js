27e93649f87fee44067cff83c3db9bd795d41346
Azir@9200 <aziruddin83@hotmail.com>
2023-09-04 09:14:11 +0100

    created index file

---
+++ js/json.js
@@ -0,0 +1,10 @@
+const hello = localStorage.getItem('hello')
+console.log('hello');
+
+// const setAge = () => {
+//   localStorage.setItem('azir age is', 323);
+// }
+
+const getNow = () => {
+  localStorage.getItem('click anchor', 7765);
+}
